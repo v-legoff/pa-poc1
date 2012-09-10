d7a922ecd95b3898412c6a45170f027e105885d1
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2012-09-10 18:34:17 +0200

    Add the model structure and their fields: >>> from model import * >>> class User(Model): ...     age = Integer() ... >>> User.fields() [<field 'id' (2)>, <field 'age' (3)>]

---
+++ model/__init__.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2012 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""This package contains the Model structure.
+
+The main class, the Model class, is defined in this file (see below).
+
+"""
+
+from model.meta import MetaModel
+from model.types import *
+
+class Model(metaclass=MetaModel):
+    
+    """Abstract class for a model.
+    
+    Each model must inherit from it.  This class provides:
+    -   Methods to create, edit, update and delete objects
+    -   Methods to find and filter objects.
+    
+    The Model class use a DataConnector object to access datas (read and write
+    it).
+    
+    Each column is defined in the class body.  For isntance:
+    >>> class User(Model):
+    ...     '''A model for an user.'''
+    ...     username = String(max_size=30)
+    ...     password = String(max_size=255)  # hashed password
+    ...     creation_date = Datetime()
+    ... 
+    
+    The 'fields' class methods gives a list of the defined attributes:
+    >>> User.fields()
+    ... [<field 'username'>, <field 'password'>, <field 'creation_date'>]
+    
+    Methods defined (which can be redefined):
+        create() -- create a new object
+        save() -- save the object through the data connector
+        delete() -- delete the object
+        find(identifier) -- get an object through its ID
+        filter(...) -- retrieve one or more object
+    
+    """
+    
+    id = Integer(key=True)
+    
+    @classmethod
+    def fields(cls):
+        """Return a list of the defined fields in this class."""
+        fields = [getattr(cls, name) for name in dir(cls)]
+        fields = [field for field in fields if isinstance(field, BaseType)]
+        fields = sorted(fields, key=lambda field: field.nid)
+        return fields
+    
+    def __init__(self, **kwargs):
+        """Create an object from keyword parameters.
+        
+        This method SHOULD NOT be redefined in a subclass.
+        
+        """
+        pass
---
+++ model/types.py
@@ -0,0 +1,80 @@
+# Copyright (c) 2012 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""This module contains the data types supported by Python Aboard.
+
+Each field is defined in a class inheited from BaseType (see below).
+
+"""
+
+import copy
+
+class BaseType:
+    
+    """Class representing an abstract type of field.
+    
+    Each created field has a 'nid' attribute, which represent its identifier
+    different from all other types, no matter the field type or the model
+    in which it is defined.  This identifier is used to order thefields
+    for an object.
+    
+    """
+    
+    current_nid = 1
+    
+    @classmethod
+    def next_nid(cls):
+        nid = BaseType.current_nid
+        BaseType.current_nid += 1
+        return nid
+    
+    def __init__(self):
+        """The basetype field constructor."""
+        self.nid = self.next_nid()
+        self.field_name = "unknown name"
+    
+    def __repr__(self):
+        return "<field {} ({})>".format(repr(self.field_name), self.nid)
+    
+    def copy(self):
+        """Return a shallow copy of self."""
+        copied = copy.copy(self)
+        copied.nid = self.next_nid()
+        return copied
+
+class Integer(BaseType):
+    
+    """Field type: integer.
+    
+    This type of field handle an integer (positive, null or negative).  It
+    DOES NOT handle floating numbers (see Float below).
+    
+    """
+    
+    def __init__(self, key=False):
+        BaseType.__init__(self)
